a26dc0773f0e2ffed5574ae0532fa334ec59dd34
Edwino Stein <edwino.stein@gmail.com>
2015-06-18 00:15:56 -0400

    Adicionando suporte a arestas

--- dev/js/app/modules/View/Canvas.js
+++ dev/js/app/modules/View/Canvas.js
@@ -1,19 +1,40 @@
 App.define('View.Canvas', {
    
     lastX: 0,
-    lastY: 0,    
+    lastY: 0,
     svgEl: null,
     
     getScale: function(){
         return this.get('View.Navigation').getScale();
     },
-       
+    
+    getVertice: function(value){
+        return this.get('View.SvgRaster').getVertice(value);
+    },
+    
+    calculateEdgePosition: function(origin, target){
+        
+        var originR = parseFloat(origin.attr('r')) + parseFloat(origin.attr('stroke-width')),
+            targetR = parseFloat(target.attr('r')) + parseFloat(target.attr('stroke-width')),
+            vector;
+    
+        origin = new Point(origin.attr('cx'), origin.attr('cy'));
+        target = new Point(target.attr('cx'), target.attr('cy'));
+        vector = new Vector(origin, target);
+        
+        return [
+            new Point(origin.x + vector.unity.x*originR, origin.y + vector.unity.y*originR),
+            new Point(target.x - vector.unity.x*targetR, target.y - vector.unity.y*targetR)
+        ];
+    },
+   
     onDragStart: function(el, e){
         if(el.hasClass('vertice')){
             var circule = el.select('circle');
             this.lastX = circule.attr('cx');
             this.lastY = circule.attr('cy');
             el.addClass('dragging');
+            this.hideEdgeWeight(el);
         }
     },
     
@@ -30,13 +51,105 @@ App.define('View.Canvas', {
                 x: +this.lastX + dx * scale,
                 y: +this.lastY + dy * scale
             });
+            
+            this.updateEdges(el);
         }
     },
     
     onDragEnds: function(el, e){
         if(el.hasClass('vertice')){
             el.removeClass('dragging');
+            this.updateEdgeWeightPosition(el);
+            this.showEdgeWeight(el);
         }
+    },
+    
+    updateEdges: function(vertice){
+        var me = this,
+            value = vertice.attr('data-value');
+        
+        $('#edges > .edge[data-origin='+value+'], #edges > .edge[data-target='+value+']').each(function(i, edge){
+            
+            var points, line = edge.querySelectorAll('line, path')[0];
+            
+            if(line.nodeName === 'line'){
+                points = me.calculateEdgePosition(
+                    me.getVertice(edge.getAttribute('data-origin')).select('circle'),
+                    me.getVertice(edge.getAttribute('data-target')).select('circle')
+                );
+
+                line.setAttribute('x1', points[0].x);
+                line.setAttribute('y1', points[0].y);
+                line.setAttribute('x2', points[1].x);
+                line.setAttribute('y2', points[1].y);
+            }
+            
+            else if(line.nodeName === 'path'){
+                
+                var circle = vertice.select('circle'),
+                    radius = parseFloat(circle.attr('r'));
+                
+                points = new Point(circle.attr('cx'), circle.attr('cy'));
+                line.setAttribute('d', 'M '+(points.x+radius)+' '+points.y+' A 1 1, 0, 0, 0, '+(points.x+7)+' '+(points.y-radius-5));
+            }
+        });
+    },
+    
+    hideEdgeWeight: function(vertice){
+        
+        var value = vertice.attr('data-value');
+        
+        $('#edges > .edge[data-origin='+value+'] > circle, #edges > .edge[data-target='+value+'] > circle').hide();
+        $('#edges > .edge[data-origin='+value+'] > text, #edges > .edge[data-target='+value+'] > text').hide();
+    },
+    
+    showEdgeWeight: function(vertice){
+        
+        var value = vertice.attr('data-value');
+        
+        $('#edges > .edge[data-origin='+value+'] > circle, #edges > .edge[data-target='+value+'] > circle').show();
+        $('#edges > .edge[data-origin='+value+'] > text, #edges > .edge[data-target='+value+'] > text').show();
+    },
+    
+    updateEdgeWeightPosition: function(vertice){
+        
+        var me = this,
+            value = vertice.attr('data-value');
+        
+        $('#edges > .edge[data-origin='+value+'], #edges > .edge[data-target='+value+']').each(function(i, edge){
+                    
+            var line = edge.querySelectorAll('line, path')[0],
+                circle = edge.querySelectorAll('circle')[0],
+                text = edge.querySelectorAll('text')[0];
+            
+            
+            if(line.nodeName === 'line'){
+                
+                var x = (parseFloat(line.getAttribute('x1')) + parseFloat(line.getAttribute('x2')))/2,
+                    y = (parseFloat(line.getAttribute('y1')) + parseFloat(line.getAttribute('y2')))/2;
+
+                circle.setAttribute('cx', x);
+                circle.setAttribute('cy', y);
+                text.setAttribute('x', x);
+                text.setAttribute('y', y);
+            }
+            
+            else if(line.nodeName === 'path'){
+                
+                var vCircle = vertice.select('circle'),
+                    radius = parseFloat(vCircle.attr('r')),
+                    point = new Point(vCircle.attr('cx'), vCircle.attr('cy'));
+                
+                point.x += radius;
+                point.y -= radius;
+                
+                circle.setAttribute('cx', point.x);
+                circle.setAttribute('cy', point.y);
+                text.setAttribute('x', point.x);
+                text.setAttribute('y', point.y);
+            }
+            
+        });
     },
     
     init: function(){
--- dev/js/app/modules/View/SvgRaster.js
+++ dev/js/app/modules/View/SvgRaster.js
@@ -21,10 +21,40 @@ App.define('View.SvgRaster',{
         'stroke': '#000',
         'stroke-width': '1px'
     },
-
+    
+    edgeLineAttr: {
+        'stroke': '#000',
+        'stroke-width': '1px',
+        'style': 'marker-start: url(#markerCircle); marker-end:url(#markerArrow)'
+    },
+    
+    edgeSelfLineAttr: {
+        'stroke': '#000',
+        'fill': 'transparent',
+        'stroke-width': '1px',
+        'style': 'marker-start: url(#markerCircle); marker-end:url(#markerArrowSelf)'
+    },
+    
+    edgeTextAttr: {
+        'font-size': 12,
+        'font-family': '"Helvetica Neue",Helvetica,Arial,sans-serif',
+        'text-anchor': 'middle',
+        'dominant-baseline': 'central',
+        'text-align': 'center'
+    },
+    
+    edgeCircleAttr: {
+        'fill': '#fff',
+        'stroke': '#000',
+        'stroke-width': '1px'
+    },
+    
     maxTextLength: 12,
     textMargin: 10,
-
+    
+    edgeTextMargin: 5,
+    maxEdgeTextLength: 6,
+    
     store:{},
 
     rasterGrid: function(){
@@ -60,9 +90,7 @@ App.define('View.SvgRaster',{
     createVerticeTextValue: function(value, x, y){
 
         value = typeof(value) === 'number' ? value.toString() : value;
-
-        if(value.length > this.maxTextLength) return null;
-
+        
         var fontSize = this.verticeTextAttr['font-size'],
             length = value.length,
             text = this.snapSvgRoot.text(x, y, value);
@@ -86,7 +114,8 @@ App.define('View.SvgRaster',{
     createVertice: function(value, x, y){
 
         value = typeof(value) === 'number' ? value.toString() : value;
-
+        if(value.length > this.maxTextLength) return null;
+        
         var g = this.snapSvgRoot.g(),
             text = this.createVerticeTextValue(value, x, y),
             circle, radius;
@@ -106,9 +135,116 @@ App.define('View.SvgRaster',{
         this.store['vertices'].add(g);
         return g;
     },
-        
+      
     getVertice: function(value){
         return this.store.vertices.select('.vertice[data-value="'+value+'"]');
+    },
+    
+    createEdgeText: function(value, x, y){
+        value = typeof(value) === 'number' ? value.toString() : value;
+        
+        var fontSize = this.edgeTextAttr['font-size'],
+            length = value.length,
+            text = this.snapSvgRoot.text(x, y, value);
+            
+        if(length >= 2) fontSize -= 3.5;
+        if(length >= 3) fontSize -= 0.5;
+        if(length >= 4) fontSize -= 1;
+        if(length >= 5) fontSize -= 1;
+        if(length >= 6) fontSize -= 1;
+        
+        for(var i in this.edgeTextAttr){
+            
+            if(i === 'font-size'){
+                text.attr('font-size', fontSize);
+                continue;
+            }
+
+            text.attr(i, this.edgeTextAttr[i]);
+        }
+        
+        return text;
+        
+    },
+    
+    createEdge: function (origin, target, weight){
+        
+        weight = typeof(weight) === 'number' ? weight.toString() : weight;
+        if(weight.length > this.maxEdgeTextLength) return null;
+        
+        var points, line, circle, radius, text, g;
+        
+        if(origin === target){
+            
+            origin = origin.select('circle');
+            radius = parseFloat(origin.attr('r'));
+            points = new Point(origin.attr('cx'), origin.attr('cy'));
+            
+            line = this.snapSvgRoot.path('M '+(points.x+radius)+' '+points.y+' A 1 1, 0, 0, 0, '+(points.x+7)+' '+(points.y-radius-5));
+            line.attr(this.edgeSelfLineAttr);
+            
+            points.x += radius;
+            points.y -= radius;
+            
+            text = this.createEdgeText(
+                weight,
+                points.x,
+                points.y
+            );
+            
+            radius = (parseFloat(text.attr('font-size'))/2) * weight.length/2 + this.edgeTextMargin;
+            circle = this.snapSvgRoot.circle(
+                points.x,
+                points.y,
+                radius
+            );
+            circle.attr(this.edgeCircleAttr);
+    
+            text.attr('transform', 'translate('+(radius)+', -'+(radius*0.2)+')');
+            circle.attr('transform', 'translate('+(radius)+', -'+(radius*0.2)+')');
+        }
+        
+        else{
+            
+            points = this.get('View.Canvas').calculateEdgePosition(
+                origin.select('circle'),
+                target.select('circle')
+            );
+
+            line = this.snapSvgRoot.line(
+                points[0].x,
+                points[0].y,
+                points[1].x,
+                points[1].y
+            );
+            line.attr(this.edgeLineAttr);
+
+
+            text = this.createEdgeText(
+                weight,
+                (points[0].x + points[1].x)/2,
+                (points[0].y + points[1].y)/2
+            );
+
+            radius = (parseFloat(text.attr('font-size'))/2) * weight.length/2 + this.edgeTextMargin;
+            circle = this.snapSvgRoot.circle(
+                (points[0].x + points[1].x)/2,
+                (points[0].y + points[1].y)/2,
+                radius
+            );
+            circle.attr(this.edgeCircleAttr);
+        }
+        
+        g = this.snapSvgRoot.g();
+        g.addClass('edge');
+        g.attr({
+            'data-origin': origin.attr('data-value'),
+            'data-target': target.attr('data-value'),
+            'data-weight': weight
+        });
+        
+        g.add(line, circle,text);
+        this.store['edges'].add(g);
     },
     
     enableDraggable: function(e){
@@ -136,9 +272,6 @@ App.define('View.SvgRaster',{
         me.store['edges'].attr('id', 'edges');
         me.store['vertices'] = this.snapSvgRoot.g();
         me.store['vertices'].attr('id', 'vertices');
-        
-        me.createVertice(1234565, 0, 0);
-        me.createVertice(2, 10, 10);
     }
 });
 
